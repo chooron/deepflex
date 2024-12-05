93525444e866085824c63e02f48af9f22b007d24
jingx <50790703+chooron@users.noreply.github.com>
2024-12-05 09:20:41 +0800

    Update make.jl

--- doc/make.jl
+++ doc/make.jl
@@ -2,25 +2,39 @@ using Documenter
 using HydroModels
 
 makedocs(
-    sitename="HydroModels.jl",
-    authors="xin jing",
-    format=Documenter.HTML(assets=["assets/icons.ico"]),
-    modules=[HydroModels],
-    checkdocs=:none,
-    pages=[
+    sitename = "HydroModels.jl",
+    authors = "xin jing",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://chooron.github.io/HydroModels.jl",
+        assets = ["assets/icons.ico"],
+        highlights = ["yaml"],
+        collapselevel = 2,
+        sidebar_sitename = false
+    ),
+    modules = [HydroModels],
+    clean = true,
+    doctest = true,
+    checkdocs = :none,
+    pages = [
         "Home" => "index.md",
+        # "Getting Started" => "getting_started.md",
         "Tutorials" => [
             "Run a Bucket Model" => "tutorials/run_a_bucket.md",
-            "Run ExpHydro Model" => "tutorials/run_a_exphydro_model.md"
-        ]
+            "Run ExpHydro Model" => "tutorials/run_a_exphydro_model.md",
+            # "Spatial Model Example" => "tutorials/spatial_model.md"
+        ],
+        # "API Reference" => [
+        #     "Model Types" => "api/types.md",
+        #     "Model Functions" => "api/functions.md",
+        #     "Utility Functions" => "api/utils.md"
+        # ],
+        # "Contributing" => "contributing.md"
     ]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=
 deploydocs(
-    repo = "github.com/USERNAME/HydroModels.jl.git"
+    repo = "github.com/chooron/HydroModels.jl",
+    devbranch = "main",
+    push_preview = true
 )
-=#
